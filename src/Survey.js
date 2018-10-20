d859f4bc68d6005a1907a17ceaa0f3ea85091274
matt <smrkem@gmail.com>
2018-10-20 13:31:04 -0400

    refactor design

--- src/Survey.js
+++ src/Survey.js
@@ -3,19 +3,26 @@ import './App.css'
 
 export default class Survey extends Component {
   state = {
+    showing: false,
     issues: [
-      {name: "Gun Control", category: "uncategorized", bgcolor: "lightblue"},
-      {name: "Animal Testing", category: "uncategorized", bgcolor: "yellow"},
-      {name: "Marriage Equality", category: "uncategorized", bgcolor: "lightgreen"},
-      {name: "Abortion", category: "uncategorized", bgcolor: "red"},
-      {name: "Hijab / Burqa", category: "uncategorized", bgcolor: "aqua"},
-      {name: "Marijuana Legalisation", category: "uncategorized", bgcolor: "orange"},
-      {name: "Gender Inequality", category: "uncategorized", bgcolor: "pink"}
+      {
+        name: "Gun Control",
+        category: "uncategorized",
+        bgcolor: "lightblue",
+        copy: "Gun Control copy placeholder..."
+      },
+      {name: "Animal Testing", copy: "Animal Testing copy placeholder...", category: "uncategorized", bgcolor: "yellow"},
+      {name: "Marriage Equality", copy: "Marriage Equality copy placeholder...", category: "uncategorized", bgcolor: "lightgreen"},
+      {name: "Abortion", copy: "Gun Control copy placeholder...", category: "uncategorized", bgcolor: "red"},
+      {name: "Hijab / Burqa", copy: "Gun Control copy placeholder...", category: "uncategorized", bgcolor: "aqua"},
+      {name: "Marijuana Legalisation", copy: "Gun Control copy placeholder...", category: "uncategorized", bgcolor: "orange"},
+      {name: "Gender Inequality", copy: "Gun Control copy placeholder...", category: "uncategorized", bgcolor: "pink"}
     ]
   }
 
   onDragStart(e, id) {
     e.dataTransfer.setData("id", id);
+    this.showIssue(id);
   }
 
   onDragOver(e) {
@@ -24,7 +31,7 @@ export default class Survey extends Component {
 
   onDrop(e, cat) {
     let id = e.dataTransfer.getData("id");
-    // console.log('drop: ', id);
+    this.setState({showing: false});
     let issues = this.state.issues.map(issue => {
       if (issue.name == id) {
         issue.category = cat;
@@ -35,6 +42,15 @@ export default class Survey extends Component {
     this.setState({issues});
   }
 
+  onClick(e, id) {
+    this.showIssue(id);
+  }
+
+  showIssue(id) {
+    let issue = this.state.issues.filter(issue => issue.name === id)[0];
+    this.setState({showing: issue.name});
+  }
+
   render() {
     var issues = {
       uncategorized: [],
@@ -42,11 +58,13 @@ export default class Survey extends Component {
       carelittle: [],
       carelots: []
     }
+    var detail = (<div></div>);
 
     this.state.issues.forEach((i) => {
       issues[i.category].push(
         <div key={i.name}
             draggable
+            onClick={e => this.onClick(e, i.name)}
             onDragStart={e => this.onDragStart(e, i.name)}
             className="draggable"
             style= {{backgroundColor: i.bgcolor}}
@@ -54,10 +72,21 @@ export default class Survey extends Component {
           {i.name}
         </div>
       )
+      if (i.name === this.state.showing) {
+        detail = (
+          <div className="issueDetailView">
+            <h3>{i.name}</h3>
+            <p>{i.copy}</p>
+          </div>
+        )
+      }
     })
     return (
       <div className="container-drag">
         <h2 className="header">SOCIAL ISSUES SURVEY</h2>
+        <div className="issueDetailView">
+          {detail}
+        </div>
         <div className="uncategorized">
             <span className="task-header">ISSUES</span>
             {issues.uncategorized}
